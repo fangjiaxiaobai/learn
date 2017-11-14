867c837ea858a42f28b9ce2144df0d98813d7f0d
fangjiaxiaobai <fangjiaxiaobai@163.com>
2017-11-14 23:16:19 +0800

    gradle学习记录

---
+++ 07gradle/demo3/src/test/java/com/fxb/main/ApplicationTest.java
@@ -0,0 +1,7 @@
+package com.fxb.main;
+
+public class Application{
+	public static void main(String []args){
+		System.out.println("Hello Gradle Test");
+	}
+}
---
+++ 07gradle/gradleIdeaDemo/src/FirstProjectByIdeaInGradle.java
@@ -0,0 +1,5 @@
+public class FirstProjectByIdeaInGradle {
+    public static void main(String []args){
+        System.out.println("Hello! this is the first project in IDEA with gradle");
+    }
+}
