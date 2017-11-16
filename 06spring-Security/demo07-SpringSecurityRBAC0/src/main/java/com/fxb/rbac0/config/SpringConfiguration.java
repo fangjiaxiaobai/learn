9c7accb29333896df95173b90bb17c59b27b2c82
fangjiaxiaobai <fangjiaxiaobai@163.com>
2017-11-16 19:33:02 +0800

    2017-11-16 修改配置

--- 06spring-Security/demo07-SpringSecurityRBAC0/src/main/java/com/fxb/rbac0/config/SpringConfiguration.java
+++ 06spring-Security/demo07-SpringSecurityRBAC0/src/main/java/com/fxb/rbac0/config/SpringConfiguration.java
@@ -1,12 +1,16 @@
 package com.fxb.rbac0.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.stereotype.Controller;
 
 import javax.sql.DataSource;
-import java.lang.reflect.Field;
+
 
 /**
  * @CLASSNAME: SpringConfiguration
@@ -21,17 +25,35 @@ import java.lang.reflect.Field;
 public class SpringConfiguration {
 
     @Autowired
-    private Element element;
+    private Environment element;
 
     //Spring 容器的配置。
+    // dataSource  数据源配置。
     @Bean
     public DataSource getDataSource(){
         DruidDataSource dataSource = new DruidDataSource();
-        dataSource.setUrl(Properti);
-        dataSource.setDriverClassName();
-        dataSource.setUsername();
-        dataSource.setPassword();
+        dataSource.setUrl(element.getProperty("jdbc.url"));
+        dataSource.setDriverClassName(element.getProperty("jdbc.driver"));
+        dataSource.setUsername(element.getProperty("jdbc.username"));
+        dataSource.setPassword(element.getProperty("jdbc.password"));
         return dataSource;
+    }
+    
+    @Bean
+    public SqlSessionFactory getSqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(getDataSource());
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        sqlSessionFactoryBean.setMapperLocations(resolver.getResources("classpath:/mybatis/*.xml"));
+        return sqlSessionFactoryBean.getObject();
+    }
+    
+    @Bean
+    org.apache.ibatis.mapping.Environment getMybatisEnvironment(){
+        org.apache.ibatis.mapping.Environment environment = new org.apache.ibatis.mapping.Environment(
+                "mybatisSqlSession",null,getDataSource()
+        );
+        return environment;
     }
 
 }
